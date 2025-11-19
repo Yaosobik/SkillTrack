65b36e972336da099e9121a8004dec6e0b1fc08e
Sergo_111 <168209084+BEBRA22222@users.noreply.github.com>
2025-11-19 21:10:53 +0300

    ralationBaseOfData
    СУЧКИ СДЕЛАЛ ВАМ ОТНОШЕНИЕ

--- app.py
+++ app.py
@@ -5,21 +5,43 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///taskonline.db'
 db = SQLAlchemy(app)
 
 
-#БД ученика
-class Discipl (db.Model):
+class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fullname = db.Column(db.String(300), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
-    group = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    students = db.relationship('Student', back_populates='group')
 
 
-#БД учитель
-class Teacher (db.Model):
+class Student(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    fullname = db.Column(db.String(300), nullable=False)
-    password = db.Column(db.String(100), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    group = db.relationship('Group', back_populates='students')
+    # связь с выполниными заданиями
+    assignments = db.relationship('Assignment', back_populates='student')
 
 
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    assignments = db.relationship('Assignment', back_populates='teacher')
+
+
+class Assignment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200))
+    description = db.Column(db.Text)
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teacher.id'))
+    teacher = db.relationship('Teacher', back_populates='assignments')
+
+
+class StudentAssignment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'))
+    assignment_id = db.relationship(db.Integer, db.ForeignKey('assignment.id'))
+    answer_text = db.Column(db.Text)
+    grade = db.Column(db.Integer)
+    student = db.relationship('Student', back_populates='assignments')
+    assignment = db.relationship('Assignment', back_populates='student')
 @app.route('/')
 def index():
     return render_template('Main.html')
