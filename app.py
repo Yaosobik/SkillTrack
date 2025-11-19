d3904c7295a14e02daf3ca2eb109c3bc86c41033
Yaosobi <magomedovdinislam05@gmail.com>
2025-11-19 20:01:05 +0300

    главная,регистрация,вход,home БЕЗ ПОДВЯЗКИ И БД

--- app.py
+++ app.py
@@ -7,7 +7,17 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route('/home')
+def grades():
+    return render_template('glavnaya.html')
+
+@app.route('/sign')
+def sign_in():
+    return render_template('sign.html')
+
+@app.route('/registration')
+def registration():
+    return render_template('registr.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
-    
